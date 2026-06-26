d0db6a0eb8055f851d1a51c3b05c57eff1efd18b
mmtrackers55-dotcom <mmtrackers55@gmail.com>
2026-06-27 02:13:45 +0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,92 @@
 
+const products = [
+
+{
+id:1,
+name:"ST-901A",
+price:6999,
+image:"images/st901a.jpg",
+description:"Real-time GPS tracker with engine cut-off and live tracking."
+},
+
+{
+id:2,
+name:"ST-901",
+price:6499,
+image:"images/st901.jpg",
+description:"Compact GPS tracker with ACC monitoring."
+},
+
+{
+id:3,
+name:"ST-901AL",
+price:6999,
+image:"images/st901al.jpg",
+description:"Advanced GPS tracker with external antenna."
+},
+
+{
+id:4,
+name:"ST-901L",
+price:6999,
+image:"images/st901l.jpg",
+description:"Live tracking and geo-fence support."
+},
+
+{
+id:5,
+name:"ST-906",
+price:7499,
+image:"images/st906.jpg",
+description:"Vehicle GPS tracker with backup battery."
+},
+
+{
+id:6,
+name:"ST-906L",
+price:7999,
+image:"images/st906l.jpg",
+description:"4G enabled GPS tracking solution."
+},
+
+{
+id:7,
+name:"ST-903",
+price:5999,
+image:"images/st903.jpg",
+description:"Personal and vehicle tracking device."
+},
+
+{
+id:8,
+name:"ST-904",
+price:6499,
+image:"images/st904.jpg",
+description:"Reliable GPS tracker with alerts."
+},
+
+{
+id:9,
+name:"ST-902 OBD",
+price:5499,
+image:"images/st902obd.jpg",
+description:"Plug-and-play OBD GPS tracker."
+},
+
+{
+id:10,
+name:"ST-905L",
+price:7999,
+image:"images/st905l.jpg",
+description:"Long battery GPS tracker."
+},
+
+{
+id:11,
+name:"ST-907 Relay",
+price:1499,
+image:"images/st907.jpg",
+description:"Relay for remote engine cut-off."
+}
+
+];
