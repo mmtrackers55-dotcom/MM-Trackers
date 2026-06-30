a321a67b3731bf14d28a151742c90058c93aee89
mmtrackers55-dotcom <mmtrackers55@gmail.com>
2026-06-30 23:13:57 +0500

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,39 +1,10 @@
 {
 id:1,
-name:"SinoTrack ST-901A Real-Time GPS Tracker for Cars & Bikes",
-
+name:"...",
+price:6999,
 images:[
-"images/901A-1.jpg",
-"images/901A-2.jpg",
-"images/901A-3.jpg",
-"images/901A-4.jpg"
+...
 ],
-description:`SinoTrack ST-901A GPS Vehicle Tracker
-
-Keep your vehicle secure with the SinoTrack ST-901A GPS Tracker, a reliable real-time vehicle monitoring device designed for cars, bikes, trucks, and fleets.
-
-Using the Android app, web platform, or SMS commands, you can monitor your vehicle anytime, anywhere with accurate live location tracking and complete vehicle status.`,
-
-features:[
-"Real-Time Live Location Tracking",
-"Vehicle Immobilizer (Remote Engine Cut-Off)",
-"ACC Ignition ON / OFF Alerts",
-"Over-Speed Alerts",
-"Geo-Fencing",
-"Shock / Vibration Alert",
-"Sleep Mode",
-"Built-in Backup Battery",
-"Battery Removal Alert",
-"Set up to 3 Admin Numbers",
-"Lifetime FREE Software",
-"Android & iOS Supported",
-"App Name: SinotrackPro",
-"Web & Mobile Tracking Available",
-"SIM Card with SMS + Internet Package",
-"Professional installation recommended",
-"Wiring diagram provided in manual",
-"Suitable for Cars",
-"Suitable for Bikes",
-"Suitable for Commercial & Personal Vehicles"
-]
-},
+description: ...,
+features:[...]
+}
