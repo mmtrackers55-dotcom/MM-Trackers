8cb8982d4eefde84a4b22ccce6e8a9649d0cf923
mmtrackers55-dotcom <mmtrackers55@gmail.com>
2026-07-06 01:14:45 +0500

    Update cart.js

--- cart.js
+++ cart.js
@@ -2,7 +2,6 @@
 function getCart() {
     return JSON.parse(localStorage.getItem("cart")) || [];
 }
-
 // Save cart
 function saveCart(cart) {
     localStorage.setItem("cart", JSON.stringify(cart));
