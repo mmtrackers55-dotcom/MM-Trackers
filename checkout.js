f8b1a27a2e6fc141283b10cab316e490e64725b6
mmtrackers55-dotcom <mmtrackers55@gmail.com>
2026-07-06 02:23:16 +0500

    Update checkout.js

--- checkout.js
+++ checkout.js
@@ -3,10 +3,35 @@ let cart = getCart();
 
 let total = 0;
 
+let html = "";
+
 cart.forEach(item=>{
+
     total += item.price * item.qty;
+
+    html += `
+    <div class="checkout-item">
+
+        <h3>${item.name}</h3>
+
+        <p>
+            Rs. ${item.price.toLocaleString()}
+            ×
+            ${item.qty}
+        </p>
+
+        <strong>
+            Subtotal:
+            Rs. ${(item.price * item.qty).toLocaleString()}
+        </strong>
+
+        <hr>
+
+    </div>
+    `;
 });
 
+document.getElementById("checkout-items").innerHTML = html;
 document.getElementById("checkout-total").innerHTML =
 "Total : Rs. " + total.toLocaleString();
 
