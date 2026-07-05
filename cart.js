6a64a542d1b155c4551364cdf2e2a691786e0a03
mmtrackers55-dotcom <mmtrackers55@gmail.com>
2026-07-06 00:47:49 +0500

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,1 +1,30 @@
-h
+// Get cart from LocalStorage
+function getCart() {
+    return JSON.parse(localStorage.getItem("cart")) || [];
+}
+
+// Save cart
+function saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
+
+// Add product
+function addToCart(product) {
+
+    let cart = getCart();
+
+    // Check if already exists
+    let existing = cart.find(item => item.name === product.name);
+
+    if (existing) {
+        existing.qty += 1;
+    } else {
+        product.qty = 1;
+        cart.push(product);
+    }
+
+    saveCart(cart);
+
+    alert(product.name + " added to cart.");
+
+}
